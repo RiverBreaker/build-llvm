eca3a03f5132e823b23118bf05b9cfc9c9c0101a
RiverBreaker <lyq203q@163.com>
2026-07-30 22:32:38 +0800

    test

--- scripts/build.py
+++ scripts/build.py
@@ -9,6 +9,11 @@ import subprocess
 import sys
 from pathlib import Path
 
+if sys.platform == "win32":
+    import io
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 def main():
     parser = argparse.ArgumentParser(description="调用 build 进行 LLVM 构建")
     parser.add_argument("--build-dir", required=True, help="构建目录")
